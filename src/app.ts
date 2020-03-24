a34e134f8abf0814e343cc87ac160eb392aa2bc3
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-03-24 10:05:21 +0800

    chore: fusion

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ const appConfig: IAppConfig = {
     rootId: 'ice-container',
     getInitialData: async () => {
       try {
-        // const data = await request('http://127.0.0.1:3333/api/app/data');
-        const data = await new Promise(resolve => {
-          setTimeout(() => {
-            resolve({ user: {username: '淘小宝啊'}})
-          }, 0.5 * 1000);
-        })
+        const data = await request('http://127.0.0.1:3333/api/app/data');
+        // const data = await new Promise(resolve => {
+        //   setTimeout(() => {
+        //     resolve({ user: {username: '淘小宝啊'}})
+        //   }, 0.5 * 1000);
+        // })
         return data;
       } catch(err) {
         console.error('getInitialData error', err);
